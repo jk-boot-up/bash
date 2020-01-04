4a507cd9417fd921aff33af3a46058fe8bc42553
jayasekhar <jayasekhar.konduru@cdk.com>
2020-01-04 19:00:49 +0530

    initial draft for set and dollar arguments

---
+++ src/dollar.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "Welcome to $ testing"
+
+echo ${1:-test me}
+
+#default 2nd argument
+echo ${2:-hello}
+
+echo ${3:-jk}
---
+++ src/set.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# set is a shell built-in that displays all shell variables & shell functions
+set
+
+# https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
+set -e
